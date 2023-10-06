249d2df3fdd73566e8fef193d4aa4080facfc855
Raguman K <rrst1010@gmail.com>
2023-10-06 16:07:03 -0400

    Cors Fix

--- Backend/capstone/capstone/settings.py
+++ Backend/capstone/capstone/settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "knox",
     "registration",
     "api",
@@ -82,6 +83,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
