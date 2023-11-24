f5b36bddde868bebb166798801426da2fd9703a4
Pratik Dahal <pratikdahal105@gmail.com>
2023-11-23 22:36:34 -0500

    media update

--- Backend/capstone/sponsor/models.py
+++ Backend/capstone/sponsor/models.py
@@ -5,7 +5,7 @@ class Sponsor(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
     name = models.CharField(max_length=200)
-    logo = models.ImageField(upload_to='media/images/uploads')
+    logo = models.ImageField(upload_to='images/uploads')
     description = models.TextField()
     amount_sponsored = models.DecimalField(max_digits=10, decimal_places=2)
     sponsor_level = models.PositiveSmallIntegerField()
