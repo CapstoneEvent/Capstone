4c3f530413ae3b70c233855cbda07507cfc7ce41
Pratik Dahal <pratikdahal105@gmail.com>
2023-11-30 20:44:29 -0500

    event and bookig for admin

--- Backend/capstone/api/admin_api/serializers.py
+++ Backend/capstone/api/admin_api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from event.models import Event
 from sponsor.models import Sponsor
 from booking.models import Booking
+from django.utils.text import slugify
+from event.models import Event
 from registration.models import Profile
 from django.contrib.auth.models import User
 
@@ -51,4 +53,119 @@ class UserWithProfileSerializer(serializers.ModelSerializer):
         profile.verified_at = profile_data.get('verified_at', profile.verified_at)
         profile.save()
 
+        return instance
+
+class EventSerializer(serializers.ModelSerializer):
+    user_email = serializers.EmailField(write_only=True)
+
+    class Meta:
+        model = Event
+        fields = (
+            'id', 'slug', 'name', 'cover_picture', 'description',
+            'start_date', 'end_date', 'available_seats',
+            'price', 'status', 'user_email'
+        )
+        extra_kwargs = {
+            'cover_picture': {'required': False},
+            'slug': {'required': False},
+        }
+
+    def validate_user_email(self, value):
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("No user found with this email")
+        return value
+
+    def create(self, validated_data):
+        user_email = validated_data.pop('user_email')
+        user = User.objects.get(email=user_email)
+        event = Event(**validated_data)
+        event.save()
+        event.user.add(user)
+        slug = slugify(event.name)
+        unique_slug = slug
+        num = 1
+        while Event.objects.filter(slug=unique_slug).exists():
+            unique_slug = f'{slug}-{num}'
+            num += 1
+
+        event.slug = unique_slug
+        event.save()
+
+        return event
+
+    def update(self, instance, validated_data):
+        user_email = validated_data.pop('user_email', None)
+
+        if user_email:
+            user = User.objects.get(email=user_email)
+            instance.user.clear()
+            instance.user.add(user)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.cover_picture = validated_data.get('cover_picture', instance.cover_picture)
+        instance.description = validated_data.get('description', instance.description)
+        instance.start_date = validated_data.get('start_date', instance.start_date)
+        instance.end_date = validated_data.get('end_date', instance.end_date)
+        instance.available_seats = validated_data.get('available_seats', instance.available_seats)
+        instance.price = validated_data.get('price', instance.price)
+        instance.status = validated_data.get('status', instance.status)
+
+        if 'name' in validated_data:
+            slug = slugify(validated_data['name'])
+            if instance.slug != slug:
+                unique_slug = slug
+                num = 1
+                while Event.objects.filter(slug=unique_slug).exists():
+                    unique_slug = f'{slug}-{num}'
+                    num += 1
+                instance.slug = unique_slug
+
+        instance.save()
+        return instance
+
+class EventSlugField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return Event.objects.get(slug=data)
+        except Event.DoesNotExist:
+            raise serializers.ValidationError(f"Event with slug '{data}' does not exist.")
+
+class BookingSerializer(serializers.ModelSerializer):
+    event = EventSlugField(queryset=Event.objects.all(), slug_field='slug')
+    user_email = serializers.EmailField(write_only=True)  #
+
+    class Meta:
+        model = Booking
+        fields = ['event', 'quantity', 'total', 'status', 'user_email'] 
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Quantity must be greater than 0.")
+        return value
+
+    def create(self, validated_data):
+        user_email = validated_data.pop('user_email')
+
+        try:
+            user = User.objects.get(email=user_email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User with provided email does not exist.")
+
+        event = validated_data['event']
+        quantity = validated_data['quantity']
+
+        total = quantity * event.price
+        validated_data['total'] = total
+        validated_data['user'] = user
+
+        return Booking.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        if 'quantity' in validated_data:
+            instance.total = validated_data['quantity'] * instance.event.price
+        else:
+            instance.total = validated_data.get('total', instance.total)
+        
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
         return instance
--- Backend/capstone/api/admin_api/views.py
+++ Backend/capstone/api/admin_api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
-from .serializers import UserWithProfileSerializer
+from .serializers import UserWithProfileSerializer, EventSerializer, BookingSerializer
 from api.user_api.decorators import require_authenticated_and_valid_token as valid_token
 from .decorators import check_admin_status
+from event.models import Event
+from booking.models import Booking
 
 
 @api_view(["GET"])
@@ -51,3 +53,82 @@ def user_detail(request, id):
         user.delete()
         return Response({"status": True, "message": "User deleted.", "data": None}, status=204)
 
+@api_view(['GET', 'POST'])
+@valid_token
+@check_admin_status
+def event_list_create(request):
+    if request.method == 'GET':
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response({"status": True, "message": "Events retrieved.", "data": serializer.data})
+
+    elif request.method == 'POST':
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": True, "message": "New event created.", "data": serializer.data})
+        return Response({"status": False, "message": "Event creation failed.", "data": serializer.errors}, status=400)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+@valid_token
+@check_admin_status
+def event_detail(request, id):
+    try:
+        event = Event.objects.get(id=id)
+    except Event.DoesNotExist:
+        return Response({"status": False, "message": "Event not found.", "data": None}, status=404)
+
+    if request.method == 'GET':
+        serializer = EventSerializer(event)
+        return Response({"status": True, "message": "Event retrieved.", "data": serializer.data})
+
+    elif request.method == 'PUT':
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": True, "message": "Event updated.", "data": serializer.data})
+        return Response({"status": False, "message": "Event update failed.", "data": serializer.errors}, status=400)
+
+    elif request.method == 'DELETE':
+        event.delete()
+        return Response({"status": True, "message": "Event deleted.", "data": None}, status=204)
+
+@api_view(['GET', 'POST'])
+@valid_token
+@check_admin_status
+def booking_list_create(request):
+    if request.method == 'GET':
+        bookings = Booking.objects.select_related('event').all()
+        serializer = BookingSerializer(bookings, many=True)
+        return Response({"status": True, "message": "Bookings retrieved.", "data": serializer.data})
+
+    elif request.method == 'POST':
+        serializer = BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": True, "message": "New booking created.", "data": serializer.data})
+        return Response({"status": False, "message": "Booking creation failed.", "data": serializer.errors}, status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@valid_token
+@check_admin_status
+def booking_detail(request, id):
+    try:
+        booking = Booking.objects.get(id=id)
+    except Booking.DoesNotExist:
+        return Response({"status": False, "message": "Booking not found.", "data": None}, status=404)
+
+    if request.method == 'GET':
+        serializer = BookingSerializer(booking)
+        return Response({"status": True, "message": "Booking retrieved.", "data": serializer.data})
+
+    elif request.method == 'PUT':
+        serializer = BookingSerializer(booking, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": True, "message": "Booking updated.", "data": serializer.data})
+        return Response({"status": False, "message": "Booking update failed.", "data": serializer.errors}, status=400)
+
+    elif request.method == 'DELETE':
+        booking.delete()
+        return Response({"status": True, "message": "Booking deleted.", "data": None}, status=204)
