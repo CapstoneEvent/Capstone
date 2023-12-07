dbf1b2b7b5bbeaafc9b29aaf77c47195ea0664de
Raguman K <rrst1010@gmail.com>
2023-12-07 00:13:52 -0500

    added qr customer

--- Frontend/src/pages/BookingPage.js
+++ Frontend/src/pages/BookingPage.js
@@ -1,9 +1,11 @@
-// BookingPage.js
 import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from "@mui/material";
+import QRCode from "qrcode.react";
 
 const BookingPage = () => {
   const [bookings, setBookings] = useState([]);
+  const [selectedBooking, setSelectedBooking] = useState(null);
   const { user } = useAuthContext();
 
   useEffect(() => {
@@ -13,6 +15,7 @@ const BookingPage = () => {
         const response = await fetch("/booking/bookings/");
         if (response.ok) {
           const bookingData = await response.json();
+          console.log(bookingData);
           if (user.status === 0) {
             setBookings(bookingData.data);
           } else {
@@ -31,6 +34,14 @@ const BookingPage = () => {
     fetchBookings();
   }, []);
 
+  const handleCardClick = (booking) => {
+    setSelectedBooking(booking);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedBooking(null);
+  };
+
   return (
     <div className="container mx-auto my-8">
       <h1 className="text-2xl font-semibold mb-4">Booking Page</h1>
@@ -39,7 +50,11 @@ const BookingPage = () => {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {bookings.map((booking) => (
-            <div key={booking.id} className="bg-green-100 p-4 rounded-lg shadow-md">
+            <div
+              key={booking.id}
+              onClick={() => handleCardClick(booking)} // Added onClick handler
+              className="bg-green-100 p-4 rounded-lg shadow-md cursor-pointer"
+            >
               <p className="text-lg font-semibold mb-2">Event: {booking.event}</p>
               <p className="mb-2">Quantity: {booking.quantity}</p>
               <p className="mb-2">Total: CAD {booking.total}</p>
@@ -48,6 +63,28 @@ const BookingPage = () => {
           ))}
         </div>
       )}
+
+      {/* Modal */}
+      <Dialog open={Boolean(selectedBooking)} onClose={handleCloseModal} fullWidth maxWidth="sm">
+        <DialogTitle className="bg-green-300 text-center mb-4 font-bold">QR Code and Details</DialogTitle>
+        <DialogContent className="text-center">
+          <div className="flex flex-col items-center space-y-4">
+            {/* Generate QR code using verification_token */}
+            <QRCode value={selectedBooking?.verification_token} size={300} className="mt-4" />
+            <Typography variant="hea" paragraph className="font-bold">
+              {selectedBooking?.verification_token}
+            </Typography>
+            <Typography variant="hea" paragraph className="font-bold">
+              Quantity: {selectedBooking?.quantity}
+            </Typography>
+          </div>
+        </DialogContent>
+        <DialogActions className="justify-center mt-1 qrb">
+          <button onClick={handleCloseModal} variant="contained">
+            Close
+          </button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
